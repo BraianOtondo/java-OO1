b48a44f81a5fcaabd4887ca4272100f58bb1bbd6
BraianOtondo <braian_otondo99@outlook.com>
2021-08-21 19:16:29 -0300

    Getters y Setters de los atributos creados

--- Geometria/src/geometria/Punto.java
+++ Geometria/src/geometria/Punto.java
@@ -1,5 +1,24 @@
 package geometria;
 
 public class Punto {
+private double x;
+private double y;
+
+
+
+
+	public double getX() {
+		return x;
+	}
+	public void setX(double x) {
+		this.x = x;
+	}
+	public double getY() {
+		return y;
+	}
+	public void setY(double y) {
+		this.y = y;
+	}
+
 
 }
