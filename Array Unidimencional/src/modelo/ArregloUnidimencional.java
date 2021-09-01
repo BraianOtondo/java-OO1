ee5380d5ca99b3e6fb51a1dbc6b0397a7a95ded8
BraianOtondo <braian_otondo99@outlook.com>
2021-09-01 20:28:53 -0300

    Moda agregada

--- Array Unidimencional/src/modelo/ArregloUnidimencional.java
+++ Array Unidimencional/src/modelo/ArregloUnidimencional.java
@@ -1,5 +1,66 @@
 package modelo;
+import java.util.Arrays;
 
 public class ArregloUnidimencional {
+private int[] vector;
 
+public ArregloUnidimencional(int[] vector) {
+	this.vector = vector;
+}
+
+@Override
+public String toString() {
+	return "ArregloUnidimencional [vector=" + Arrays.toString(vector) + "]";
+}
+
+public int[] getVector() {
+	return vector;
+}
+
+public void setVector(int[] vector) {
+	this.vector = vector;
+}
+public void mostrarArrays(){
+    for(int i = 0; i <vector.length; i++){
+        System.out.println("vector ["+i+"]=" +vector[i]);
+    }
+}
+public int[] ordenar() {
+    int aux;
+    for(int i=0;i<vector.length-1;i++){
+        for(int j=0;j<vector.length-i-1;j++){
+            if(vector[j+1]<vector[j]) {
+                aux=vector[j+1];
+                vector[j+1]=vector[j];
+                vector[j]=aux;
+            }
+        }
+    }
+    return vector;
+}
+public double traerFrecuencia(int numero){
+	float contar=0;
+	for(int i=0;i<vector.length;i++){
+		if(vector[i]==numero){
+			contar++;
+		}
+	}
+	return (contar/vector.length)*100;
+}
+//+ traerModa(): int
+public int traerModa(){
+	//double max=this.traerFrecuencia(vector[0]);
+	//boolean band;
+	int pos=0;
+	int i;
+	for(i=0;i<(vector.length)-1;i++){
+		if(this.traerFrecuencia(vector[i])>this.traerFrecuencia(vector[i+1])){
+			pos=i;
+		}
+		//if(repetido!=vector[i]){
+		//}
+		
+	}
+	return vector[pos];
+}
 }
---
+++ Array Unidimencional/src/test/ArregloUnidimensionalTest.java
@@ -0,0 +1,18 @@
+package test;
+import modelo.ArregloUnidimencional;
+public class ArregloUnidimensionalTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] array={10,2,30,6,10,10};
+		ArregloUnidimencional vector1=new ArregloUnidimencional(array);
+		System.out.println("MUESTRA 1");
+		vector1.mostrarArrays();
+		vector1.ordenar();
+		System.out.println("MUESTRA 2");
+		vector1.mostrarArrays();
+		System.out.println("El numero 10 se repite con una frecuencia del %"+vector1.traerFrecuencia(10));
+		System.out.println("Lo que está de moda es :"+vector1.traerModa());
+	}
+
+}
